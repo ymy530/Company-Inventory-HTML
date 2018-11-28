9888c665a6fae5099a9cfefc05dbb4b014b57145
Ming <myu4@uwo.ca>
2018-11-27 20:01:17 -0500

    cross check

--- getorders.php
+++ getorders.php
@@ -13,39 +13,43 @@
   mysqli_free_result($result);
   }
   
- //  else($whichMus == "2"){
-//   $query = "select * from products order by cost DESC";
-//   $result = mysqli_query($connection, $query); 
-//   if (!$result) {
-//     die("databases query on art pieces failed. "); }
-//   echo "<ul>"; //put the artwork in an unordered bulleted list
-//   while($row = mysqli_fetch_assoc($result)){
-//     echo "<li>" .$row["productid"]. "  Description:  " .$row["description"]. "  Price:  " .$row["cost"]. "  Quantity:  " .$row["quantity"]. "</li>";
-//   }
-//   echo "</ul>";   //end the bulleted list
-//   }
-//   else($whichMus == "4"){
-//   $query = 'select * from products order by description ASC';
-//   $result = mysqli_query($connection, $query); 
-//   if (!$result) {
-//     die("databases query on art pieces failed. "); }
-//   echo "<ul>"; //put the artwork in an unordered bulleted list
-//   while($row = mysqli_fetch_assoc($result)){
-//     echo "<li>" .$row["productid"]. "  Description:  " .$row["description"]. "  Price:  " .$row["cost"]. "  Quantity:  " .$row["quantity"]. "</li>";
-//   }
-//   echo "</ul>";   //end the bulleted list
-//   }
+  elseif($whichMus == "2"){
+  $query = "select * from products order by cost DESC";
+  $result = mysqli_query($connection, $query); 
+  if (!$result) {
+    die("databases query on art pieces failed. "); }
+  echo "<ul>"; //put the artwork in an unordered bulleted list
+  while($row = mysqli_fetch_assoc($result)){
+    echo "<li>" .$row["productid"]. "  Description:  " .$row["description"]. "  Price:  " .$row["cost"]. "  Quantity:  " .$row["quantity"]. "</li>";
+  }
+  echo "</ul>";   //end the bulleted list
+  mysqli_free_result($result);
+  }
   
- //  else($whichMus == "3"){
-//   $query = 'select * from products order by description DESC';
-//   $result = mysqli_query($connection, $query); 
-//   if (!$result) {
-//     die("databases query on art pieces failed. "); }
-//   echo "<ul>"; //put the artwork in an unordered bulleted list
-//   while($row = mysqli_fetch_assoc($result)){
-//     echo "<li>" .$row["productid"]. "  Description:  " .$row["description"]. "  Price:  " .$row["cost"]. "  Quantity:  " .$row["quantity"]. "</li>";
-//   }
-//   echo "</ul>";   //end the bulleted list
-//   }
+  elseif($whichMus == "3"){
+  $query = 'select * from products order by description ASC';
+  $result = mysqli_query($connection, $query); 
+  if (!$result) {
+    die("databases query on art pieces failed. "); }
+  echo "<ul>"; //put the artwork in an unordered bulleted list
+  while($row = mysqli_fetch_assoc($result)){
+    echo "<li>" .$row["productid"]. "  Description:  " .$row["description"]. "  Price:  " .$row["cost"]. "  Quantity:  " .$row["quantity"]. "</li>";
+  }
+  echo "</ul>";   //end the bulleted list
+  mysqli_free_result($result);
+  }
+  
+  else($whichMus == "4"){
+  $query = 'select * from products order by description DESC';
+  $result = mysqli_query($connection, $query); 
+  if (!$result) {
+    die("databases query on art pieces failed. "); }
+  echo "<ul>"; //put the artwork in an unordered bulleted list
+  while($row = mysqli_fetch_assoc($result)){
+    echo "<li>" .$row["productid"]. "  Description:  " .$row["description"]. "  Price:  " .$row["cost"]. "  Quantity:  " .$row["quantity"]. "</li>";
+  }
+  echo "</ul>";   //end the bulleted list
+  mysqli_free_result($result);
+  }
   
   ?>
