878313884d5f5b1ef1c3616c757f52166727632a
Ming <myu4@uwo.ca>
2018-11-28 15:52:32 -0500

    change to php

--- getdescription.php
+++ getdescription.php
@@ -5,9 +5,9 @@
         die("databases query failed.");
     }  
    while ($row = mysqli_fetch_assoc($result)) {
-        echo  '<input type="radio" name="product" value="';
-        echo $row["productid"]. "'>";
-        //echo '">'  "<br>";//. $row["firstname"] . " " . $row["lastname"] .;
+       echo '<input type="radio" name="product" value="';
+        echo $row["productid"];
+        echo '">' .$row["description"].  "<br>";
    }
    mysqli_free_result($result);
 ?>
