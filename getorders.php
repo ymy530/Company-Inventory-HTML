e512968005030cd174802da6dbe06b165a0f953b
Ming <myu4@uwo.ca>
2018-11-27 19:51:53 -0500

    cross check

--- getorders.php
+++ getorders.php
@@ -12,17 +12,17 @@
   echo "</ul>";   //end the bulleted list
   }
   
-  else($whichMus == "2"){
-  $query = "select * from products order by cost DESC";
-  $result = mysqli_query($connection, $query); 
-  if (!$result) {
-    die("databases query on art pieces failed. "); }
-  echo "<ul>"; //put the artwork in an unordered bulleted list
-  while($row = mysqli_fetch_assoc($result)){
-    echo "<li>" .$row["productid"]. "  Description:  " .$row["description"]. "  Price:  " .$row["cost"]. "  Quantity:  " .$row["quantity"]. "</li>";
-  }
-  echo "</ul>";   //end the bulleted list
-  }
+ //  else($whichMus == "2"){
+//   $query = "select * from products order by cost DESC";
+//   $result = mysqli_query($connection, $query); 
+//   if (!$result) {
+//     die("databases query on art pieces failed. "); }
+//   echo "<ul>"; //put the artwork in an unordered bulleted list
+//   while($row = mysqli_fetch_assoc($result)){
+//     echo "<li>" .$row["productid"]. "  Description:  " .$row["description"]. "  Price:  " .$row["cost"]. "  Quantity:  " .$row["quantity"]. "</li>";
+//   }
+//   echo "</ul>";   //end the bulleted list
+//   }
 //   else($whichMus == "4"){
 //   $query = 'select * from products order by description ASC';
 //   $result = mysqli_query($connection, $query); 
