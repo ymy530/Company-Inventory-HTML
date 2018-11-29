d5d0b870a7d7059ebcac5ddca80177987e44a8c3
Ming <myu4@uwo.ca>
2018-11-28 19:12:41 -0500

    tryQ4

--- a3.php
+++ a3.php
@@ -79,13 +79,14 @@ Purchase Quantity: <input type="text" name="quantity"><br>
     Agent: <select name="customeragent" id = "customeragent"><br>
     <option value="1"> Select Here </option>
 <?php
-      include "connectdb.php";
+    if (isset($_POST['customeragent'])) { 
+      include "connectdb.php"; 
       include "getagentid.php";
+    } 
 ?>
 
         </select>
-        <br>
-        <br>
+        <br><br>
   <input type="submit" value="Click here to add New Customer">
   <hr>
 </form>
