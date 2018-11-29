1339f4e98e10b1b0ce2282f64035782b7c55dfaf
Ming <myu4@uwo.ca>
2018-11-28 19:48:18 -0500

    tryQ4

--- getagentid.php
+++ getagentid.php
@@ -1,7 +1,7 @@
 <?php
 	//$whichAgent = $_POST["customeragent"];
 // Get the customer table order by last name
-   $query = 'SELECT  * FROM agents';
+   $query = "SELECT  * FROM agents";
    // run the query
    $result = mysqli_query($connection,$query);
    if (!$result) {
