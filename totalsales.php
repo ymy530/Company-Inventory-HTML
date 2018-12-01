5cada17edce5f7649e1bcfcf6d1b44037da378aa
Ming <myu4@uwo.ca>
2018-12-01 08:44:38 -0500

    tryQ4

--- totalsales.php
+++ totalsales.php
@@ -2,7 +2,7 @@
    include 'connectdb.php';
    $whichproduct= $_POST["totalsalesproduct"];//fetch the value that user selected
    //return the product purchased correspond to value
-   $query = 'SELECT description, sum(purchasequantity) AS totalsale, (cost*purchasequantity) AS money FROM products, purchased WHERE products.productid = purchased.productid AND purchased.productid = "' .$whichproduct. '"';
+   $query = 'SELECT description, sum(purchasequantity) AS totalsale, (cost*sum(purchasequantity)) AS money FROM products, purchased WHERE products.productid = purchased.productid AND purchased.productid = "' .$whichproduct. '"';
    $result=mysqli_query($connection,$query);
     if (!$result) {
          die("database query2 failed.");
